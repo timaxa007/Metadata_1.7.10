78d4e6c10b5a93d03b1f53477c1f95d5b280395b
timaxa007 <timaxa007@gmail.com>
2015-03-27 14:44:42 +0300

    Update 27.03.2015 (2)

--- java/metadata/ItemBlockMetadata.java
+++ java/metadata/ItemBlockMetadata.java
@@ -12,22 +12,12 @@ public class ItemBlockMetadata extends ItemBlock {
 		setHasSubtypes(true);
 	}
 
-	public ItemBlockMetadata() {
-		super(ModMetadata.block_metadata);
-		setMaxDamage(0);
-		setHasSubtypes(true);
-	}
-
 	public String getUnlocalizedName(ItemStack is) {
 		int metadata = is.getItemDamage();
 		if (metadata >= 0 && metadata < BlockMetadata.metadata.length) {
 			return super.getUnlocalizedName() + "." + BlockMetadata.metadata[metadata];
 		}
 		return super.getUnlocalizedName();
-	}
-
-	public int getMetadata(int metadata) {
-		return metadata;
 	}
 
 }
