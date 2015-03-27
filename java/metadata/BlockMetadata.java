57cae9f1c8de607efcbe9d99142ea0779c0b5f9e
timaxa007 <timaxa007@gmail.com>
2015-03-27 16:11:30 +0300

    Update 27.03.2015 (3)

--- java/metadata/BlockMetadata.java
+++ java/metadata/BlockMetadata.java
@@ -1,11 +1,15 @@
 package metadata;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
@@ -47,6 +51,16 @@ public class BlockMetadata extends Block {
 		setBlockName("block_metadata");
 	}
 
+	public int quantityDropped(Random random) {
+		return 0;
+	}
+
+	public ArrayList<ItemStack> getDrops(World world, int x, int y, int z, int metadata, int fortune) {
+		ArrayList<ItemStack> ret = new ArrayList<ItemStack>();
+		ret.add(new ItemStack(ModMetadata.block_metadata, 1, metadata));
+		return ret;
+	}
+
 	public int idPicked(World world, int x, int y, int z) {
 		return 0;
 	}
@@ -55,9 +69,18 @@ public class BlockMetadata extends Block {
 		return new ItemStack(world.getBlock(x, y, z), 1, world.getBlockMetadata(x, y, z));
 	}
 
+	@Override
+	public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase entity, ItemStack is) {
+		world.setBlockMetadataWithNotify(x, y, z, is.getItemDamage(), 3);
+	}
+
+
 	@SideOnly(Side.CLIENT)
 	public IIcon getIcon(int side, int metadata) {
-		return icon_array[metadata];
+		if (metadata >= 0 && metadata < BlockMetadata.metadata.length) {
+			return icon_array[metadata];
+		}
+		return super.getIcon(side, metadata);
 	}
 
 	@SideOnly(Side.CLIENT)
@@ -69,8 +92,8 @@ public class BlockMetadata extends Block {
 
 	@SideOnly(Side.CLIENT)
 	public void registerBlockIcons(IIconRegister ir) {
+		super.registerBlockIcons(ir);
 		icon_array = new IIcon[metadata.length];
-		blockIcon = ir.registerIcon(getTextureName());
 		for (int i = 0; i < metadata.length; ++i) {
 			icon_array[i] = ir.registerIcon(getTextureName() + "/" + metadata[i]);
 		}
