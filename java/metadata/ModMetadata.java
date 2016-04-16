71c6ea4aa8ff8f0c0779f015ca12dcc21efb1fea
timaxa007 <timaxa007@gmail.com>
2016-04-17 00:40:47 +0300

    doUpdate

--- java/metadata/ModMetadata.java
+++ java/metadata/ModMetadata.java
@@ -14,7 +14,7 @@ public class ModMetadata {
 
 	public static final String MODID = "metadata";
 	public static final String MODNAME = "Metadata";
-	public static final String VERSION = "0.1a";
+	public static final String VERSION = "0.2a";
 
 	@Instance(ModMetadata.MODID)
 	public static ModMetadata instance;
